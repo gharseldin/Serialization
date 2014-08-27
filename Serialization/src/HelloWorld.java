3ab6461d497b8ba3368e26104713b7d3f9ed212a
Amr Gharseldin <gharseldin@aucegypt.edu>
2014-08-27 18:56:29 -0400

    This is the second Commit with a new line
    new line to clarify
    and a comment on the first print

--- Serialization/src/HelloWorld.java
+++ Serialization/src/HelloWorld.java
@@ -3,7 +3,11 @@ public class HelloWorld {
 
 	public static void main(String [] args){
 		
+		//added this on second revision to clarify
 		System.out.println("This is just a test class to see what's going on");
+		
+		//Here is the second line
+		System.out.println("This is just another line to see how the new changes compare");
 	}
 	
 }
