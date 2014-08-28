a1ea708bc3d3c7eeab763bcc4d4fb38ed18e80c9
Amr Gharseldin <gharseldin@aucegypt.edu>
2014-08-28 00:11:56 -0400

    This is the whole working Quiz reader application

--- Serialization/src/QuizCardBuilder.java
+++ Serialization/src/QuizCardBuilder.java
@@ -2,8 +2,14 @@ import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -20,106 +26,116 @@ public class QuizCardBuilder {
 	private JTextArea question;
 	private JTextArea answer;
 
-	// private ArrayList<QuizCard> cardList;
+	private ArrayList<QuizCard> cardList;
 
 	public static void main(String[] args) {
 		QuizCardBuilder builder = new QuizCardBuilder();
 		builder.go();
 	}
 
-	public void go(){
-		
+	public void go() {
+
 		frame = new JFrame("Quiz Card Builder");
 		JPanel mainPanel = new JPanel();
-		
+
 		Font bigFont = new Font("sansserif", Font.BOLD, 24);
-		
+
 		question = new JTextArea(6, 20);
 		question.setLineWrap(true);
 		question.setWrapStyleWord(true);
 		question.setFont(bigFont);
-		
+
 		JScrollPane qScroller = new JScrollPane(question);
-		qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		
+		qScroller
+				.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		qScroller
+				.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
 		answer = new JTextArea(6, 20);
 		answer.setLineWrap(true);
 		answer.setWrapStyleWord(true);
 		answer.setFont(bigFont);
-		
+
 		JScrollPane aScroller = new JScrollPane(answer);
-		aScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		aScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		
+		aScroller
+				.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		aScroller
+				.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
 		JButton nextButton = new JButton("Next Card");
-		
-	//	cardList = new ArrayList<QuizCard>();
-		
-		JLabel qLabel  = new JLabel("Question:");
-		JLabel aLabel  = new JLabel("Answer:");
-		
+
+		cardList = new ArrayList<QuizCard>();
+
+		JLabel qLabel = new JLabel("Question:");
+		JLabel aLabel = new JLabel("Answer:");
+
 		mainPanel.add(qLabel);
 		mainPanel.add(qScroller);
 		mainPanel.add(aLabel);
 		mainPanel.add(aScroller);
 		mainPanel.add(nextButton);
-		
-		//define Button Action
+
+		// define Button Action
 		nextButton.addActionListener(new NextCardListener());
-		
+
 		JMenuBar menuBar = new JMenuBar();
 		JMenu fileMenu = new JMenu("File");
-		
+
 		JMenuItem newMenuItem = new JMenuItem("New");
 		JMenuItem saveMenuItem = new JMenuItem("Save");
 		newMenuItem.addActionListener(new NewMenuListener());
 		saveMenuItem.addActionListener(new SaveMenuListener());
-		
+
 		fileMenu.add(newMenuItem);
 		fileMenu.add(saveMenuItem);
-		
+
 		JMenu helpMenu = new JMenu("Help");
-		
+
 		JMenuItem aboutMenuItem = new JMenuItem("About");
 		JMenuItem versionMenuItem = new JMenuItem("Version");
 		aboutMenuItem.addActionListener(new AboutMenuListener());
 		versionMenuItem.addActionListener(new VersionMenuListener());
-		
+
 		helpMenu.add(aboutMenuItem);
 		helpMenu.add(versionMenuItem);
-		
-		//Add both menus to the MenuBar now
+
+		// Add both menus to the MenuBar now
 		menuBar.add(fileMenu);
 		menuBar.add(helpMenu);
-		
-		//Add the MenuBar to the Frame
+
+		// Add the MenuBar to the Frame
 		frame.setJMenuBar(menuBar);
-		
+
 		frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
-		frame.setSize(500,600);
+		frame.setSize(500, 600);
 		frame.setVisible(true);
-		
+
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		
+
 	}
 
 	class NextCardListener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent event) {
-			// TODO
-			// Add action to the next button
+			QuizCard card = new QuizCard(question.getText(), answer.getText());
+			cardList.add(card);
+			clearScreen();
 		}
+	}
+
+	private void clearScreen() {
+		question.setText("");
+		answer.setText("");
+		question.requestFocus();
 	}
 
 	class NewMenuListener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent event) {
-			// TODO
-			// Add action to menu item
+			cardList.clear();
+			clearScreen();
 		}
 	}
 
@@ -127,11 +143,37 @@ public class QuizCardBuilder {
 
 		@Override
 		public void actionPerformed(ActionEvent event) {
-			// TODO
-			// Add action to menu item
+			QuizCard card = new QuizCard(question.getText(), answer.getText());
+			cardList.add(card);
+
+			JFileChooser fileSave = new JFileChooser();
+			fileSave.showSaveDialog(frame);
+			saveFile(fileSave.getSelectedFile());
 		}
 	}
-	
+
+	public void saveFile(File file) {
+
+		try {
+			FileWriter fw = new FileWriter(file);
+			BufferedWriter writer = new BufferedWriter(fw);
+
+			for (QuizCard x : cardList) {
+				writer.write(x.getQuestion());
+				writer.write("/");
+				writer.write(x.getAnswer());
+				writer.write("\n");
+			}
+
+			writer.close();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
 	class AboutMenuListener implements ActionListener {
 
 		@Override
@@ -140,7 +182,7 @@ public class QuizCardBuilder {
 			// Add action to menu item
 		}
 	}
-	
+
 	class VersionMenuListener implements ActionListener {
 
 		@Override
