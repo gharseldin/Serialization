da521f1b5b681a315975249d2f11ec10abca1b91
Amr Gharseldin <gharseldin@aucegypt.edu>
2014-08-27 23:04:57 -0400

    Version 1.1 with the menu installed with the file and help submenu items

--- Serialization/src/QuizCardBuilder.java
+++ Serialization/src/QuizCardBuilder.java
@@ -1,29 +1,32 @@
 import java.awt.BorderLayout;
 import java.awt.Font;
-import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
-
 
 public class QuizCardBuilder {
 
 	private JFrame frame;
 	private JTextArea question;
 	private JTextArea answer;
-//	private ArrayList<QuizCard> cardList;
-	
-	
-	public static void main(String [] args){
+
+	// private ArrayList<QuizCard> cardList;
+
+	public static void main(String[] args) {
 		QuizCardBuilder builder = new QuizCardBuilder();
 		builder.go();
 	}
-	
+
 	public void go(){
 		
 		frame = new JFrame("Quiz Card Builder");
@@ -53,14 +56,35 @@ public class QuizCardBuilder {
 		
 	//	cardList = new ArrayList<QuizCard>();
 		
-		JLabel qLabel = qLabel = new JLabel("Question:");
-		JLabel aLabel = aLabel = new JLabel("Anser");
+		JLabel qLabel  = new JLabel("Question:");
+		JLabel aLabel  = new JLabel("Answer:");
 		
 		mainPanel.add(qLabel);
 		mainPanel.add(qScroller);
 		mainPanel.add(aLabel);
 		mainPanel.add(aScroller);
 		mainPanel.add(nextButton);
+		
+		//define Button Action
+		nextButton.addActionListener(new NextCardListener());
+		
+		JMenuBar menuBar = new JMenuBar();
+		JMenu fileMenu = new JMenu("File");
+		
+		JMenuItem newMenuItem = new JMenuItem("New");
+		JMenuItem saveMenuItem = new JMenuItem("Save");
+		newMenuItem.addActionListener(new NewMenuListener());
+		saveMenuItem.addActionListener(new SaveMenuListener());
+		
+		
+		JMenu helpMenu = new JMenu("Help");
+		
+		JMenuItem aboutMenuItem = new JMenuItem("About");
+		JMenuItem versionMenuItem = new JMenuItem("Version");
+		aboutMenuItem.addActionListener(new AboutMenuListener());
+		versionMenuItem.addActionListener(new VersionMenuListener());
+		
+		
 		
 		
 		frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
@@ -70,5 +94,50 @@ public class QuizCardBuilder {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		
+	}
+
+	class NextCardListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent event) {
+			// TODO
+			// Add action to the next button
+		}
+	}
+
+	class NewMenuListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent event) {
+			// TODO
+			// Add action to menu item
+		}
+	}
+
+	class SaveMenuListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent event) {
+			// TODO
+			// Add action to menu item
+		}
+	}
+	
+	class AboutMenuListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent event) {
+			// TODO
+			// Add action to menu item
+		}
+	}
+	
+	class VersionMenuListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent event) {
+			// TODO
+			// Add action to menu item
+		}
 	}
 }
